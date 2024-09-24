fddada93d87d7ad1c3b0f3f2dfb035cf95bc6f52
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2024-09-24 19:01:38 +0400

    #11 fixed promise return to distinguish actions

--- packages/fe/src/lib/utils/feedback/confirm-msg.js
+++ packages/fe/src/lib/utils/feedback/confirm-msg.js
@@ -3,13 +3,14 @@ import { ElMessageBox } from 'element-plus';
 export const $confirm = (message, title, options = {}) => {
     return new Promise((resolve, reject) => {
       ElMessageBox.confirm(message, title, {
+        distinguishCancelAndClose: true,
         confirmButtonText: options.confirmButtonText || 'OK',
         cancelButtonText: options.cancelButtonText || 'Cancel',
         type: options.type || 'warning', // types ('info', 'success', 'warning')
         ...options
       })
-        .then(() => resolve(true))   
-        .catch(() => reject(false)); 
+        .then(resolve)   
+        .catch(reject); 
     });
 };
 
