bc5c81dd6422212a4fa02ba2bcecb1891ac0ee0d
AkerkeMilano <79296697+AkerkeMilano@users.noreply.github.com>
2024-09-25 16:02:53 +0400

    11 confirm notify utils (#20)
    11 created confirm and notify utils

---
+++ packages/fe/src/lib/utils/feedback/confirm-msg.js
@@ -0,0 +1,16 @@
+import { ElMessageBox } from 'element-plus';
+
+export const $confirm = (message, title, options = {}) => {
+    return new Promise((resolve, reject) => {
+      ElMessageBox.confirm(message, title, {
+        distinguishCancelAndClose: true,
+        confirmButtonText: options.confirmButtonText || 'OK',
+        cancelButtonText: options.cancelButtonText || 'Cancel',
+        type: options.type || 'warning', // types ('info', 'success', 'warning')
+        ...options
+      })
+        .then(resolve)   
+        .catch(reject); 
+    });
+};
+
---
+++ packages/fe/src/lib/utils/feedback/notify-msg.js
@@ -0,0 +1,18 @@
+import { ElNotification } from 'element-plus';
+
+export const $notify = (options = {}) => {
+    ElNotification({
+      title: options.title || 'Notification', 
+      message: options.message || '',
+      type: options.type || 'info', // types ('info', 'success', 'warning')
+      ...options, 
+    });
+};
+
+export const $notifyUserAboutError = (error) => {
+    ElNotification({
+        title: 'Error', 
+        message: error,
+        type: 'error'
+      });
+}
