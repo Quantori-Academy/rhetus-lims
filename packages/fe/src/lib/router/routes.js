ef33ca73d3f34dc2d2acf1d1acf1245c6387f1b8
Anna <128999856+Anna1-991@users.noreply.github.com>
2024-09-26 16:23:42 +0400

    1 added create new user page
    see pr #23

--- packages/fe/src/lib/router/routes.js
+++ packages/fe/src/lib/router/routes.js
@@ -13,5 +13,10 @@ export const routes = [
 		path: '/users/list',
 		name: 'users-list',
 		component: () => import('../../users/list/user-list.vue')
+	},
+	{
+		path: '/users/new',
+		name: 'new-user',
+		component: () => import('../../users/new/add-new-user.vue')
 	}
 ];
