3082c8c42210b824d33fcd0bbf104ea092a3f92b
Boris <6479500dev@gmail.com>
2024-09-28 00:18:01 +0400

    #14 cleanup, added updates and migrations

--- packages/be/src/lib/db/migrate.js
+++ packages/be/src/lib/db/migrate.js
@@ -1,29 +1,29 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { migrate } from "drizzle-orm/node-postgres/migrator";
-import pg from "pg";
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { migrate } from 'drizzle-orm/node-postgres/migrator';
+import pg from 'pg';
 
 import { getConfig } from '../config/config.js';
-import config from '../../../drizzle.config.js'
+import config from '../../../drizzle.config.js';
 
 const { database } = getConfig();
 
 const pool = new pg.Pool({
-  connectionString: database.pg.url,
+	connectionString: database.pg.url
 });
 
 const db = drizzle(pool);
 
 async function main() {
-  if (config.out) {
-    await migrate(db, { migrationsFolder: config.out });
-    console.log("Migration done!");
-  }
+	if (config.out) {
+		await migrate(db, { migrationsFolder: config.out });
+		console.log('Migration done!');
+	}
 }
 
 main()
-  .catch((err) => {
-    console.error(err);
-  })
-  .finally(async () => {
-    await pool.end();
-  });
+	.catch(err => {
+		console.error(err);
+	})
+	.finally(async () => {
+		await pool.end();
+	});
--- packages/be/src/lib/db/schema/index.js
+++ packages/be/src/lib/db/schema/index.js
@@ -1,11 +1,7 @@
-import { cities } from './cities.js';
-import { countries } from './countries.js';
 import { users } from './users.js';
 import { roles } from './roles.js';
 
 export const schema = {
-	cities,
-	countries,
 	users,
 	roles
 };
