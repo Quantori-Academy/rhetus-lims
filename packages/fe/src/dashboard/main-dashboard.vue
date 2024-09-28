d13412638bd0b9967021b61c3a1e0a807aff0ff3
Karina <umiralikarina@gmail.com>
2024-09-28 18:46:00 +0500

    3 reinstalled dependencies

--- packages/fe/src/dashboard/main-dashboard.vue
+++ packages/fe/src/dashboard/main-dashboard.vue
@@ -58,7 +58,7 @@ const handleSidebarOpen = () => {
 			</el-menu-item>
 		</el-menu>
 	</div>
-
+	
 	<div v-if="!menu" class="header">
 		<el-row class="show-row">
 			<el-button circle @click="handleSidebarOpen" >
