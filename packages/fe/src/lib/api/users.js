2d39eb694a15a30a6827523ae5349e77102b1e20
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2024-09-30 17:19:12 +0400

    8 add user detail page (admin only) (#24)
    - Added user details form with toggle functionality for editing.
    - Implemented form validations and button for password change requests.
    - Fixed input types and added necessary refs for better functionality.
    - Adjusted form width for responsive design.
    - Updated user route from user-details to users/:id.
    - Refactored API calls and validation processes.
    - Integrated MSW for user retrieval and editing.
    - Addressed node version issues and dependency fixes.
    - Cleaned up unnecessary changes outside the scope of this PR.

--- packages/fe/src/lib/api/users.js
+++ packages/fe/src/lib/api/users.js
@@ -7,5 +7,20 @@ export default http => ({
 			method: 'POST',
 			body: user
 		});
-	}
+	},
+	async fetchUser(id) {
+		return await http(`/users/${id}`);
+	},
+	async updateUser(id, user) {
+		return await http(`/users/${id}`, {
+			method: 'PUT',
+			body: JSON.stringify(user)
+		});
+	},
+	async changePassword(id, confirm) {
+    return await http(`/users/${id}/change-password`, {
+        method: 'POST',
+        body: JSON.stringify({ confirm })
+    });
+}
 });
