1d5af2fb488b1be949fcb73bb20aecfc0efbdb86
Karina <umiralikarina@gmail.com>
2024-10-06 23:03:50 +0500

    4 added user profile page

--- packages/fe/src/profile/user-profile.vue
+++ packages/fe/src/profile/user-profile.vue
@@ -1,11 +1,14 @@
 <script setup>
-import {onMounted, ref} from 'vue';
+import {onMounted, ref, useTemplateRef} from 'vue';
 import { $api } from '../lib/api';
 import { ElForm, ElFormItem, ElInput, ElButton } from 'element-plus';
-import { $notifyUserAboutError } from '../lib/utils/feedback/notify-msg';
+import { $notifyUserAboutError, $notify } from '../lib/utils/feedback/notify-msg';
+import { router } from '../lib/router/router';
 
 const editable = ref(false);
 const profile = ref(null);
+const initialData = ref(null);
+const form = useTemplateRef('form');
 
 const props = defineProps({
 	id: {
@@ -15,14 +18,50 @@ const props = defineProps({
 });
 const setProfile = async id => {
 	try {
-		const data = await $api.users.fetchUser(id);
-		profile.value = data;
+		const data = profile.value = await $api.users.fetchUser(id);
+		initialData.value = {...data};
 	} catch(err) {
-		$notifyUserAboutError(err.message)
+		$notifyUserAboutError(err)
 	}
 };
+const ifCancel = () => {
+	profile.value = {...initialData.value};
+	editable.value = !editable.value;
+};
+const ifEdit = () => {
+	editable.value = !editable.value;
+};
+const requiredRule = {required: true, message: 'Length should be at least 1', trigger: 'blur'};
+const validation = ref({
+	firstName: [requiredRule],
+	lastName: [requiredRule],
+	email: [{required: true, type: 'email', message: 'Please input correct email address', trigger: 'blur'}]
+});
+async function validate() {
+	return form.value.validate();
+};
 const onProfileEdit = () => {
-	editable.value = !editable.value
+	if (editable.value) {
+		ifCancel();
+	} else {
+		ifEdit();
+	}
+};
+const editHandler = async () => {
+	if (!(await validate())) return;
+	try {
+		await $api.users.updateUser(props.id, profile.value)
+		editable.value = false;
+		$notify({
+			title: 'Success',
+			message: 'Profile updated',
+			type: 'success'})
+	} catch (err) {
+		$notifyUserAboutError(err)
+	}
+};
+const passwordResetHandler = () => {
+	router.push({name: 'reset-password'})
 };
 onMounted(() => {
 	setProfile(props.id)
@@ -33,12 +72,20 @@ onMounted(() => {
 <template>
 	<div class="profile">
 		<h1>Profile</h1>
-		<el-form v-if="profile" class="el-form" :model="profile" label-position="top" >
+		<el-form
+			v-if="profile"
+			ref="form"
+			class="el-form"
+			:model="profile"
+			label-position="top"
+			:rules="validation"
+			@submit="editHandler"
+			>
 			<el-form-item label="Username" prop="username">
-				<el-input v-model="profile.username" readonly=true :disabled="editable" />
+				<el-input v-model="profile.username" :readonly=true :disabled="editable" />
 			</el-form-item>
 			<el-form-item label="First name" prop="firstName">
-				<el-input v-model="profile.firstName" :readonly=!editable />
+				<el-input v-model="profile.firstName" :readonly="!editable" />
 			</el-form-item>
 			<el-form-item label="Last name" prop="lastName">
 				<el-input v-model="profile.lastName" :readonly=!editable />
@@ -46,15 +93,13 @@ onMounted(() => {
 			<el-form-item label="Email" prop="email">
 				<el-input v-model="profile.email" :readonly=!editable />
 			</el-form-item>
-			<!-- <el-form-item label="Password">
-				<el-input type="password" :readonly=!editable />
-			</el-form-item> -->
 			<el-form-item label="Role" prop="role">
-				<el-input v-model="profile.role" readonly=true :disabled=editable />
+				<el-input v-model="profile.role" :readonly=true :disabled=editable />
 			</el-form-item>
-
 			<el-form-item>
-				<el-button :type="editable ? 'default' : 'primary'" @click="onProfileEdit">{{ editable ? 'Cancel' : 'Edit' }}</el-button>
+				<el-button :type="editable ? 'default' : 'primary'" @click="onProfileEdit" >{{ editable ? 'Cancel' : 'Edit' }}</el-button>
+				<el-button v-if="editable" type="primary" @click="editHandler">Save</el-button>
+				<el-button type="warning" @click="passwordResetHandler">Reset password</el-button>
 			</el-form-item>
 		</el-form>
 	</div>
@@ -63,21 +108,17 @@ onMounted(() => {
 <style scoped>
 .profile {
 	margin: 0 15px;
-
 	h1 {
 		color: #080808;
 		margin-bottom: 1rem;
 	}
 }
-
 .el-form {
 	width: 400px;
 }
-
 @media (max-width: 520px) {
 	.el-form {
 		width: 220px;
 	}
 }
-
 </style>
