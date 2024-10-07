e1519900115089db917472775a6ad802911390f9
Karina <umiralikarina@gmail.com>
2024-10-07 23:00:51 +0500

    4 revert changes to packages/be/.env.example

--- packages/fe/src/profile/user-profile.vue
+++ packages/fe/src/profile/user-profile.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {onMounted, ref, useTemplateRef} from 'vue';
+import { onMounted, ref, useTemplateRef } from 'vue';
 import { $api } from '../lib/api';
 import { ElForm, ElFormItem, ElInput, ElButton } from 'element-plus';
 import { $notifyUserAboutError, $notify } from '../lib/utils/feedback/notify-msg';
@@ -18,28 +18,35 @@ const props = defineProps({
 });
 const setProfile = async id => {
 	try {
-		const data = profile.value = await $api.users.fetchUser(id);
-		initialData.value = {...data};
-	} catch(err) {
-		$notifyUserAboutError(err)
+		const data = (profile.value = await $api.users.fetchUser(id));
+		initialData.value = { ...data };
+	} catch (err) {
+		$notifyUserAboutError(err);
 	}
 };
 const ifCancel = () => {
-	profile.value = {...initialData.value};
+	profile.value = { ...initialData.value };
 	editable.value = !editable.value;
 };
 const ifEdit = () => {
 	editable.value = !editable.value;
 };
-const requiredRule = {required: true, message: 'Length should be at least 1', trigger: 'blur'};
+const requiredRule = { required: true, message: 'Length should be at least 1', trigger: 'blur' };
 const validation = ref({
 	firstName: [requiredRule],
 	lastName: [requiredRule],
-	email: [{required: true, type: 'email', message: 'Please input correct email address', trigger: 'blur'}]
+	email: [
+		{
+			required: true,
+			type: 'email',
+			message: 'Please input correct email address',
+			trigger: 'blur'
+		}
+	]
 });
 async function validate() {
 	return form.value.validate();
-};
+}
 const onProfileEdit = () => {
 	if (editable.value) {
 		ifCancel();
@@ -50,23 +57,23 @@ const onProfileEdit = () => {
 const editHandler = async () => {
 	if (!(await validate())) return;
 	try {
-		await $api.users.updateUser(props.id, profile.value)
+		await $api.users.updateUser(props.id, profile.value);
 		editable.value = false;
 		$notify({
 			title: 'Success',
 			message: 'Profile updated',
-			type: 'success'})
+			type: 'success'
+		});
 	} catch (err) {
-		$notifyUserAboutError(err)
+		$notifyUserAboutError(err);
 	}
 };
 const passwordResetHandler = () => {
-	router.push({name: 'reset-password'})
+	router.push({ name: 'reset-password' });
 };
 onMounted(() => {
-	setProfile(props.id)
+	setProfile(props.id);
 });
-
 </script>
 
 <template>
@@ -80,24 +87,26 @@ onMounted(() => {
 			label-position="top"
 			:rules="validation"
 			@submit="editHandler"
-			>
+		>
 			<el-form-item label="Username" prop="username">
-				<el-input v-model="profile.username" :readonly=true :disabled="editable" />
+				<el-input v-model="profile.username" :readonly="true" :disabled="editable" />
 			</el-form-item>
 			<el-form-item label="First name" prop="firstName">
 				<el-input v-model="profile.firstName" :readonly="!editable" />
 			</el-form-item>
 			<el-form-item label="Last name" prop="lastName">
-				<el-input v-model="profile.lastName" :readonly=!editable />
+				<el-input v-model="profile.lastName" :readonly="!editable" />
 			</el-form-item>
 			<el-form-item label="Email" prop="email">
-				<el-input v-model="profile.email" :readonly=!editable />
+				<el-input v-model="profile.email" :readonly="!editable" />
 			</el-form-item>
 			<el-form-item label="Role" prop="role">
-				<el-input v-model="profile.role" :readonly=true :disabled=editable />
+				<el-input v-model="profile.role" :readonly="true" :disabled="editable" />
 			</el-form-item>
 			<el-form-item>
-				<el-button :type="editable ? 'default' : 'primary'" @click="onProfileEdit" >{{ editable ? 'Cancel' : 'Edit' }}</el-button>
+				<el-button :type="editable ? 'default' : 'primary'" @click="onProfileEdit">{{
+					editable ? 'Cancel' : 'Edit'
+				}}</el-button>
 				<el-button v-if="editable" type="primary" @click="editHandler">Save</el-button>
 				<el-button type="warning" @click="passwordResetHandler">Reset password</el-button>
 			</el-form-item>
@@ -109,12 +118,12 @@ onMounted(() => {
 .profile {
 	margin: 0 15px;
 	h1 {
-		color: #080808;
 		margin-bottom: 1rem;
+		color: #080808;
 	}
 }
 .el-form {
-	width: 400px;
+	width: 500px;
 }
 @media (max-width: 520px) {
 	.el-form {
