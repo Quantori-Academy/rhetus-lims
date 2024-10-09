c739c0b116dc6111fe0daacad39e2cfe7b8ba0a4
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2024-10-09 16:05:13 +0400

    43 removed code left after conflict

--- packages/fe/src/lib/router/layout/main-layout.vue
+++ packages/fe/src/lib/router/layout/main-layout.vue
@@ -8,14 +8,6 @@ import SidebarMenu from '../../../sidebar/sidebar-menu.vue';
 		<div class="table-logo">
 			<img width="42" height="42" src="../../../lib//assets/images/logo.svg" alt="" />
 		</div>
-		<nav class="main-navigation">
-			<router-link to="/">Table</router-link>
-			<router-link to="/form">Form</router-link>
-			<router-link to="/users/list">Users</router-link>
-			<router-link to="/users/:id">User Details</router-link>
-			<router-link to="/users/list">Users</router-link>
-			<router-link to="/storages/list">Storage Locations</router-link>
-		</nav>
 		<router-view></router-view>
 	</div>
 </template>
@@ -25,18 +17,6 @@ import SidebarMenu from '../../../sidebar/sidebar-menu.vue';
 	display: grid;
 	place-content: center;
 	gap: 4px;
-}
-
-.main-navigation {
-	display: flex;
-	justify-content: center;
-	gap: 10px;
-	margin-bottom: 20px;
-
-	a {
-		color: black;
-		font-size: 16px;
-	}
 }
 
 .table-logo {
