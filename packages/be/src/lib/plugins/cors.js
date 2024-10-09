60428bf4e0210935e7edaf2174d72ad063138f6f
1ilit <1ilit@proton.me>
2024-10-09 10:37:59 +0400

    67 set up credentials and origin for cors (#70)
    * 67 set up credentials and origin for cors
    
    * 67 add CLIENT_URL to build workflow

--- packages/be/src/lib/plugins/cors.js
+++ packages/be/src/lib/plugins/cors.js
@@ -1,7 +1,22 @@
 import cors from '@fastify/cors';
+import { getConfig } from '../config/config.js';
+
+const config = getConfig();
 
 export const autoConfig = {
-	methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+	origin: (origin, cb) => {
+		const incomingUrl = new URL(origin);
+		const allowedUrl = new URL(config.client.url);
+
+		if (incomingUrl.hostname === 'localhost' || incomingUrl.origin === allowedUrl.origin) {
+			cb(null, true);
+			return;
+		}
+
+		cb(new Error('Not allowed'), false);
+	},
+	credentials: true
 };
 
 export default cors;
