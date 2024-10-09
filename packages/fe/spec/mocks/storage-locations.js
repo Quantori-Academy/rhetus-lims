0ee9354259843675f77232c94d107e34b4e19434
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2024-10-09 16:06:20 +0400

    43 updated mock and fetch api

--- packages/fe/spec/mocks/storage-locations.js
+++ packages/fe/spec/mocks/storage-locations.js
@@ -1,46 +1,44 @@
 import { http, HttpResponse } from 'msw';
 import { api } from './api-url.js';
 
-const locations = [
-    {
-        id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b4t5y5y5y',
-        room: 'Building 1, Room 12',
-        name: 'Cabinet 1, shelf 3'
-    },
-    {
-        id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b93f5g6d6d4g6g',
-        room: 'Building 2, Room 23',
-        name: 'Cabinet 2, shelf 4'
-    },
-]
+const storageInfo = {
+	storages: [
+		{
+			id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b4t5y5y5y',
+			room: 'Building 1, Room 12',
+			name: 'Cabinet 1, shelf 3',
+			description: 'This storage is located near the pool'
+		},
+		{
+			id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b93f5g6d6d4g6g',
+			room: 'Building 2, Room 23',
+			name: 'Cabinet 2, shelf 4'
+		}
+	],
+	count: 1
+};
 
 export const storageLocationHandlers = [
-    http.get(api('/storages'), () => {
-        return HttpResponse.json(locations)
-    }),
-    http.get(api('/storages/:id'), (req) => {
-        const { id } = req.params;
-        const storage = locations.find(location => location.id === id);
-        if(!storage) {
-            return HttpResponse.json(
-                {message: 'Storage location is not found'},
-                { status: 404 }
-            )
-        }
-        return HttpResponse.json(storage);
-    }),
-    http.put(api('/storages/:id'), async ({ request, params }) => {
-        const { id } = params;
-        const storage = locations.find(location => location.id === id);
-        if(!storage) {
-            return HttpResponse.json(
-                {message: 'Storage location is not found'},
-                { status: 404 }
-            )
-        }
+	http.get(api('/storages'), () => {
+		return HttpResponse.json(storageInfo);
+	}),
+	http.get(api('/storages/:id'), req => {
+		const { id } = req.params;
+		const storage = storageInfo.storages.find(location => location.id === id);
+		if (!storage) {
+			return HttpResponse.json({ message: 'Storage location is not found' }, { status: 404 });
+		}
+		return HttpResponse.json(storage);
+	}),
+	http.put(api('/storages/:id'), async ({ request, params }) => {
+		const { id } = params;
+		const storage = storageInfo.storages.find(location => location.id === id);
+		if (!storage) {
+			return HttpResponse.json({ message: 'Storage location is not found' }, { status: 404 });
+		}
 
-        const updatedStorage = await request.json();
-        Object.assign(storage, updatedStorage);
-        return HttpResponse.json(storage);
-    })
-]
+		const updatedStorage = await request.json();
+		Object.assign(storage, updatedStorage);
+		return HttpResponse.json(storage);
+	})
+];
--- packages/fe/src/lib/api/storage-locations.js
+++ packages/fe/src/lib/api/storage-locations.js
@@ -1,19 +1,19 @@
 export default http => ({
-    async fetchStorages() {
-        return await http('/storages');
-    },
-    async fetchStorage(id) {
-        return await http(`/storages/${id}`)
-    },
-    async updateStorage(id, storage) {
-        return await http(`/storages/${id}`, {
-            method: 'PUT',
-            body: storage
-        })
-    },
-    async deleteStorage(id) {
+	async fetchStorages() {
+		return await http('/storages');
+	},
+	async fetchStorage(id) {
+		return await http(`/storages/${id}`);
+	},
+	async updateStorage(id, storage) {
+		return await http(`/storages/${id}`, {
+			method: 'PUT',
+			body: storage
+		});
+	},
+	async deleteStorage(id) {
 		return await http(`/storages/${id}`, {
 			method: 'DELETE'
 		});
 	}
-})
+});
