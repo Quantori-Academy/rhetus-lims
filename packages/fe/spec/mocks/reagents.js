f2c66767888e8fe19f82a41a12bf4a6c120e5c4e
catherineisonline <ekaterine.jpg@gmail.com>
2024-10-10 10:56:40 +0400

    Add view/edit form, fake data for msw, additional validation before trashin reagent

--- packages/fe/spec/mocks/reagents.js
+++ packages/fe/spec/mocks/reagents.js
@@ -29,6 +29,65 @@ const reagents = [
 		structure: 'O=[Mn](=O)(=O)=O[O-].[K+]'
 	}
 ];
+const reagentDetails = [
+	{
+		id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
+		name: 'Sodium Chloride',
+		category: 'Reagent',
+		description: 'Common salt used in various chemical reactions and as a preservative.',
+		casNumber: '7647-14-5',
+		producer: 'Chemical Co.',
+		catalogId: 'CH-001234',
+		catalogLink: 'http://chemicalco.com/catalog/sodium-chloride',
+		unitSize: '500 g bottle',
+		unitPrice: 12.5,
+		quantityLeft: 25.0,
+		storageLocation: {
+			id: 1,
+			name: 'Shelf A1'
+		},
+		expirationDate: '2025-05-15T00:00:00.000Z'
+	},
+	{
+		id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b322',
+		name: 'Acetic Acid',
+		category: 'Reagent',
+		description:
+			'A colorless liquid organic compound with a pungent smell used in the production of various chemicals.',
+		casNumber: '64-19-7',
+		producer: 'Pure Chemicals',
+		catalogId: 'PC-002567',
+		catalogLink: 'http://purechemicals.com/catalog/acetic-acid',
+		unitSize: '1 L bottle',
+		unitPrice: 8.99,
+		quantityLeft: 15.0,
+		storageLocation: {
+			id: 2,
+			name: 'Cabinet B2'
+		},
+		expirationDate: '2024-11-30T00:00:00.000Z'
+	},
+	{
+		id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b999',
+		name: 'Acetic Acid',
+		category: 'Reagent',
+		description:
+			'A colorless liquid organic compound with a pungent smell used in the production of various chemicals.',
+		casNumber: '64-19-7',
+		producer: 'Pure Chemicals',
+		catalogId: 'PC-002567',
+		catalogLink: 'http://purechemicals.com/catalog/acetic-acid',
+		unitSize: '1 L bottle',
+		unitPrice: 8.99,
+		quantityLeft: 15.0,
+		storageLocation: {
+			id: 2,
+			name: 'Cabinet B2'
+		},
+		expirationDate: '2024-11-30T00:00:00.000Z'
+	}
+];
+
 export const reagentsHandlers = [
 	http.get(api('/reagents'), () => {
 		return HttpResponse.json(reagents);
@@ -42,6 +101,7 @@ export const reagentsHandlers = [
 	}),
 	http.delete(api('/reagents/:id'), async ({ params }) => {
 		const { id } = params;
+		console.log(id);
 		const reagentIndex = reagents.findIndex(reagent => reagent.id === id);
 		if (reagentIndex === -1) {
 			return HttpResponse.json(
@@ -57,5 +117,26 @@ export const reagentsHandlers = [
 			status: 'success',
 			message: `Reagent was deleted`
 		});
+	}),
+	http.get(api('/reagents/:id'), req => {
+		const { id } = req.params;
+		const reagent = reagentDetails.find(rg => rg.id === id);
+		if (reagent) {
+			return HttpResponse.json(reagent);
+		} else {
+			return HttpResponse.json({ message: 'Reagent not found' }, { status: 404 });
+		}
+	}),
+
+	http.put(api('/reagents/:id'), async ({ request, params }) => {
+		const { id } = params;
+		const updatedReagent = await request.json();
+		const reagent = reagentDetails.find(rg => rg.id === id);
+		if (reagent) {
+			Object.assign(reagent, updatedReagent);
+			return HttpResponse.json(reagent);
+		} else {
+			return HttpResponse.json({ message: 'Reagent not found' }, { status: 404 });
+		}
 	})
 ];
---
+++ packages/fe/src/reagents/details-reagent/reagent-details.vue
@@ -0,0 +1,210 @@
+<script setup>
+import { ElForm, ElInput, ElButton, ElFormItem, ElSelect, ElOption, ElImage } from 'element-plus';
+import { $notifyUserAboutError, $notify } from '../../lib/utils/feedback/notify-msg';
+import { $confirm } from '../../lib/utils/feedback/confirm-msg.js/';
+import { computed, onMounted, useTemplateRef } from 'vue';
+import { ref } from 'vue';
+import { $api } from '../../lib/api/index.js';
+import { $router } from '../../lib/router/router';
+import { useRoute } from 'vue-router';
+import { formatDate } from '../../lib/utils/datetime/date-format.js';
+
+const editingForm = ref(false);
+const formEl = useTemplateRef('form-ref');
+const reagent = ref(null);
+const loading = ref(true);
+
+const props = defineProps({
+	id: {
+		type: String,
+		default: null
+	},
+	isEdit: {
+		type: Boolean,
+		default: false
+	}
+});
+
+const route = useRoute();
+
+const isEdit = computed(() => {
+	return props.isEdit || route.query.isEdit === 'true';
+});
+
+onMounted(() => {
+	setReagent(props.id);
+	editingForm.value = isEdit.value;
+});
+
+const rules = ref({
+	quantityLeft: [{ required: true, message: "Quantity can't be empty" }],
+	storageLocation: [
+		{ required: true, message: "Storage location can't be empty", trigger: 'change' },
+		{
+			validator: (_, value, callback) => {
+				if (!value.name || value.name.trim() === '') {
+					callback(new Error("Storage location can't be empty"));
+				} else {
+					callback();
+				}
+			},
+			trigger: 'change'
+		}
+	]
+});
+
+const setReagent = async id => {
+	try {
+		const data = await $api.reagents.fetchReagent(id);
+		reagent.value = { ...data };
+	} catch (error) {
+		$notifyUserAboutError(error.message || 'Error updating reagent');
+	} finally {
+		loading.value = false;
+	}
+};
+
+const toggleEdit = () => {
+	editingForm.value = !editingForm.value;
+	$router.push({
+		name: 'reagent-details',
+		params: { id: reagent.value.id },
+		query: { isEdit: true }
+	});
+};
+
+const cancelEdit = () => {
+	editingForm.value = false;
+	setReagent(props.id);
+	$router.push({ name: 'reagent-details', params: { id: props.id } });
+};
+
+async function validate() {
+	return formEl.value.validate();
+}
+
+const handleSubmit = async () => {
+	const valid = await validate();
+	if (!valid) return;
+	try {
+		const updatedReagent = await $api.reagents.updateReagent(reagent.value.id, reagent.value);
+		reagent.value = updatedReagent;
+		$notify({
+			title: 'Success',
+			message: 'Reagent has been updated',
+			type: 'success'
+		});
+		toggleEdit();
+	} catch (error) {
+		$notifyUserAboutError(error.message || 'Error updating reagent');
+	}
+};
+
+const deleteReagent = async () => {
+	cancelEdit();
+	try {
+		await $confirm('Do you want to delete this reagent?', 'Warning', {
+			confirmButtonText: 'OK',
+			cancelButtonText: 'Cancel',
+			type: 'warning'
+		});
+		const response = await $api.reagents.deleteReagent(props.id);
+		console.log(response.message);
+		$notify({
+			title: 'Success',
+			message: response.message,
+			type: 'success'
+		});
+		await $router.push({ name: 'reagents-list' });
+	} catch {
+		$notify({
+			title: 'Canceled',
+			message: 'Reagent deletion canceled',
+			type: 'info'
+		});
+	}
+};
+</script>
+
+<template>
+	<div class="reagent-details">
+		<h1>Reagent Details</h1>
+		<el-form
+			v-if="reagent && !loading"
+			ref="form-ref"
+			label-position="top"
+			:model="reagent"
+			:rules="rules"
+			@submit="handleSubmit"
+		>
+			<el-form-item label="Expiration date" prop="expirationDate">
+				<span> {{ reagent.id }}</span>
+			</el-form-item>
+			<el-form-item label="Name" prop="name">
+				<el-input v-model="reagent.name" :disabled="true" />
+			</el-form-item>
+			<el-form-item label="Category">
+				<el-select v-model="reagent.category" :disabled="true" :placeholder="reagent.category">
+					<el-option label="Reagent" value="reagent" />
+					<el-option label="Sample" value="sample" />
+				</el-select>
+			</el-form-item>
+			<el-form-item label="Description" prop="description">
+				<el-input v-model="reagent.description" :disabled="true" />
+			</el-form-item>
+			<el-form-item label="Structure">
+				<el-image
+					src="https://www.thoughtco.com/thmb/hg7HpLu7rYj7tTae5CmOq7uwbKA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/flavonol-58b5f2373df78cdcd816fecf.png"
+					alt="Potassium Permanganate Structure"
+					fit="cover"
+				/>
+			</el-form-item>
+			<el-form-item label="CAS number" prop="casNumber">
+				<el-input v-model="reagent.casNumber" :disabled="true" />
+			</el-form-item>
+			<el-form-item label="Producer" prop="producer">
+				<el-input v-model="reagent.producer" :disabled="true" />
+			</el-form-item>
+			<el-form-item label="Catalog ID" prop="catalogId">
+				<el-input v-model="reagent.catalogId" :disabled="true" />
+			</el-form-item>
+			<el-form-item label="Catalog link" prop="catalogLink">
+				<el-input v-model="reagent.catalogLink" :disabled="true" />
+			</el-form-item>
+			<el-form-item label="Storage location" prop="storageLocation">
+				<el-input v-model="reagent.storageLocation.name" :disabled="!editingForm" />
+			</el-form-item>
+			<el-form-item label="Size (unit)" prop="unitSize">
+				<el-input v-model="reagent.unitSize" :disabled="true" />
+			</el-form-item>
+			<el-form-item label="Quantity" prop="quantityLeft">
+				<el-input v-model="reagent.quantityLeft" :disabled="!editingForm" />
+			</el-form-item>
+			<el-form-item label="Price per unit" prop="unitPrice">
+				<el-input v-model="reagent.unitPrice" :disabled="true" />
+			</el-form-item>
+			<el-form-item label="Expiration date" prop="expirationDate">
+				<span> {{ formatDate(reagent.expirationDate) }}</span>
+			</el-form-item>
+			<el-button v-if="editingForm" type="primary" @click="handleSubmit">{{ 'Save' }}</el-button>
+			<el-button v-else type="primary" @click="toggleEdit">{{ 'Edit reagent' }}</el-button>
+			<el-button v-if="editingForm" @click="cancelEdit">Cancel</el-button>
+			<el-button type="danger" @click="deleteReagent">{{ 'Delete reagent' }}</el-button>
+		</el-form>
+		<div v-else>Loading reagent data...</div>
+	</div>
+</template>
+
+<style scoped>
+.reagent-details {
+	display: flex;
+	flex-direction: column;
+	gap: 5rem;
+	padding-bottom: 5rem;
+	color: black;
+}
+.reagent-details :deep(.el-input__wrapper),
+.reagent-details :deep(.el-select__wrapper) {
+	background-color: transparent;
+}
+</style>
