6c9d3a871c7d308f0ff86c903b2347e2c198ce31
Karina <umiralikarina@gmail.com>
2024-10-10 22:09:17 +0500

    52 notification removed

--- packages/fe/src/lib/utils/form-validation/is-form-valid.js
+++ packages/fe/src/lib/utils/form-validation/is-form-valid.js
@@ -1,10 +1,7 @@
-import { $notifyUserAboutError } from '../feedback/notify-msg';
-
 export async function $isFormValid(formEl) {
 	try {
 		return await formEl.value.validate();
 	} catch {
-		$notifyUserAboutError('Error submitting form');
 		return false;
 	}
 }
