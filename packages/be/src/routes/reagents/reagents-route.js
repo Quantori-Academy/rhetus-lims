e83f6ed753cd80f8b33cb0969976a0a2acb70cc5
Boris <6479500dev@gmail.com>
2024-10-10 14:26:54 +0400

    #41 added reagent route, schema, service

---
+++ packages/be/src/routes/reagents/reagents-route.js
@@ -0,0 +1,89 @@
+import fp from 'fastify-plugin';
+
+import * as schema from './reagents-schema.js';
+import reagentsService from '../../services/reagents/reagents-service.js';
+
+async function reagents(server, options) {
+	await server.register(reagentsService);
+
+	server.route({
+		method: 'POST',
+		path: options.prefix + 'reagents',
+		preValidation: [server.authenticate],
+		schema: schema.createReagent,
+		handler: onCreateReagent
+	});
+
+	async function onCreateReagent(req, reply) {
+		try {
+			// TODO: uncomment location checking when storage management will be done
+			// const isLocationExist = await server.locationsService.getLocationById(req.body.storageLocationId);
+
+			// if (!isLocationExist) {
+			// 	return reply.code(409).send({ status: 'error', message: `No such storage location` });
+			// }
+
+			const reagentName = await server.reagentsService.createReagent(req.body);
+
+			return reply
+				.code(201)
+				.send({ status: 'success', message: `Reagent '${reagentName}' was created` });
+		} catch (err) {
+			return reply.code(500).send(err);
+		}
+	}
+
+	server.route({
+		method: 'GET',
+		path: options.prefix + 'reagents/:id',
+		preValidation: [server.authenticate],
+		schema: schema.getReagent,
+		handler: onGetReagent
+	});
+
+	async function onGetReagent(req, reply) {
+		try {
+			const reagentUUID = req.params.id;
+
+			const reagent = await server.reagentsService.getReagentById(reagentUUID);
+
+			if (!reagent) {
+				return reply.code(404).send({ status: 'error', message: `No such reagent` });
+			}
+
+			return reply.code(200).send({ ...reagent, category: 'reagent' });
+		} catch (err) {
+			return reply.code(500).send(err);
+		}
+	}
+
+	server.route({
+		method: 'DELETE',
+		path: options.prefix + 'reagents/:id',
+		preValidation: [server.authenticate],
+		schema: schema.deleteReagent,
+		handler: onDeleteReagent
+	});
+
+	async function onDeleteReagent(req, reply) {
+		try {
+			const reagentUUID = req.params.id;
+
+			const reagent = await server.reagentsService.getReagentById(reagentUUID);
+
+			if (!reagent) {
+				return reply.code(404).send({ status: 'error', message: `No such reagent` });
+			}
+
+			const reagentName = await server.reagentsService.softDeleteReagent(reagentUUID);
+
+			return reply
+				.code(200)
+				.send({ status: 'success', message: `Reagent '${reagentName}' was deleted` });
+		} catch (err) {
+			return reply.code(500).send(err);
+		}
+	}
+}
+
+export default fp(reagents);
---
+++ packages/be/src/routes/reagents/reagents-schema.js
@@ -0,0 +1,56 @@
+import S from 'fluent-json-schema';
+
+const statusMessage = S.object()
+	.prop('status', S.string().required())
+	.prop('message', S.string().required());
+
+const Reagent = S.object()
+	.prop('id', S.string().format(S.FORMATS.UUID).required())
+	.prop('name', S.string().minLength(1).required())
+	.prop('casNumber', S.string().minLength(1).required())
+	.prop('producer', S.string().minLength(1).required())
+	.prop('catalogId', S.string().minLength(1).required())
+	.prop('catalogLink', S.string().format(S.FORMATS.URL).required())
+	.prop('unitPrice', S.number().required())
+	.prop('quantityUnit', S.string().minLength(1).required())
+	.prop('size', S.number().required())
+	.prop('quantityLeft', S.number().required())
+	.prop('expirationDate', S.string().format(S.FORMATS.DATE_TIME).required())
+	.prop('description', S.string().required())
+	// TODO: Check is it number or string(UUID) after location management will be implemented
+	.prop('storageLocationId', S.number().required())
+	.prop('category', S.string().required());
+
+const createReagent = {
+	security: [{ Session: [] }],
+	body: Reagent.without(['id', 'category']),
+	response: {
+		201: statusMessage,
+		409: statusMessage,
+		500: statusMessage
+	}
+};
+
+const getReagent = {
+	security: [{ Session: [] }],
+	params: S.object().prop('id', S.string()),
+	response: {
+		200: Reagent.without(['storageLocationId']),
+		// TODO: import storage schema and use it here after implementation
+		// .prop('storageLocation', StorageLocation).required(),
+		404: statusMessage,
+		500: statusMessage
+	}
+};
+
+const deleteReagent = {
+	security: [{ Session: [] }],
+	params: S.object().prop('id', S.string()),
+	response: {
+		200: statusMessage,
+		404: statusMessage,
+		500: statusMessage
+	}
+};
+
+export { createReagent, getReagent, deleteReagent };
