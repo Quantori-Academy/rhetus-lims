56f9a9c8bd27816473c0e1ed601dc31f7c019cdc
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2024-10-11 12:25:43 +0400

    43 fixed prettier and max char value

--- packages/fe/src/storage-management/helpers.js
+++ packages/fe/src/storage-management/helpers.js
@@ -3,11 +3,10 @@ import { ref } from 'vue';
 export const rules = ref({
 	room: [
 		{ required: true, message: "Room can't be empty" },
-		{ max: 30, message: 'Room cannot exceed 30 characters' }
+		{ max: 300, message: 'Room cannot exceed 300 characters' }
 	],
 	name: [
 		{ required: true, message: "Name can't be empty" },
-		{ max: 30, message: 'Name cannot exceed 30 characters' }
+		{ max: 300, message: 'Name cannot exceed 300 characters' }
 	]
 });
-
