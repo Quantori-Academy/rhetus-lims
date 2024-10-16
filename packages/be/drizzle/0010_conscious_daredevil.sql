2e0f6534897d7ed9501df6fa88eb2e2ac38b65b4
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2024-10-16 13:32:00 +0400

    40 added storage migrations

--- packages/be/drizzle/0010_conscious_daredevil.sql
+++ packages/be/drizzle/0010_conscious_daredevil.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS "storages" (
-	"id" serial PRIMARY KEY NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
 	"room" varchar(300) NOT NULL,
 	"name" varchar(300) NOT NULL,
 	"description" text,
 	"created_at" timestamp NOT NULL
 );
+  
