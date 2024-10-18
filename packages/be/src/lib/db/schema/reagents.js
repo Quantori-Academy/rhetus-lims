c4386dcf966fec9265ceba7999f752eef64231ab
Boris <6479500dev@gmail.com>
2024-10-18 19:15:52 +0400

    #41 updated reagents schema, added migration

--- packages/be/src/lib/db/schema/reagents.js
+++ packages/be/src/lib/db/schema/reagents.js
@@ -9,26 +9,23 @@ import {
 	boolean
 } from 'drizzle-orm/pg-core';
 import { sql } from 'drizzle-orm';
+import { storages } from './storages.js';
 
 export const reagents = pgTable('reagents', {
 	id: uuid('id')
 		.primaryKey()
 		.default(sql`uuid_generate_v4()`),
 	name: varchar('name', { length: 256 }).notNull(),
-	// TODO: add structure in next milestones
-	// structure: text('structure').notNull(),
 	casNumber: varchar('cas_number', { length: 12 }).default(null),
-	producer: varchar('producer', { length: 256 }).notNull(),
+	producer: varchar('producer', { length: 256 }).default(null),
 	catalogId: varchar('catalog_id', { length: 256 }).default(null),
 	catalogLink: text('catalog_link').default(null),
-	unitPrice: real('unit_price').notNull(),
+	unitPrice: real('unit_price').notNull().default(0),
 	quantityUnit: varchar('quantity_unit', { length: 256 }).notNull(),
-	quantity: integer('quantity').notNull(),
+	quantity: real('quantity').notNull(),
 	quantityLeft: real('quantity_left').notNull(),
-	expirationDate: timestamp('expiration_date').notNull(),
-	storageLocationId: integer('storage_location_id'),
-	// TODO: add reference to storage, when it will be done
-	// .references(() => locations.id),
+	expirationDate: timestamp('expiration_date').default(null),
+	storageLocationId: uuid('storage_location_id').references(() => storages.id),
 	description: text('description').default(''),
 	deleted: boolean('deleted').default(false)
 });
