e48768f177557eee7b493c0891c2113d2a87a196
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2024-10-20 23:17:46 +0400

    45 added substances mock to get by storage id

--- packages/fe/spec/mocks/browser/handlers.js
+++ packages/fe/spec/mocks/browser/handlers.js
@@ -3,11 +3,13 @@ import { usersHandlers } from '../users.js';
 import { storageLocationHandlers } from '../storage-locations.js';
 import { reagentsHandlers } from '../reagents.js';
 import { authHandlers } from '../auth.js';
+import { substancesHandlers } from '../substances.js';
 
 export const handlers = [
 	...authHandlers,
 	...usersHandlers,
 	...samplesHandlers,
 	...storageLocationHandlers,
-	...reagentsHandlers
+	...reagentsHandlers,
+	...substancesHandlers
 ];
