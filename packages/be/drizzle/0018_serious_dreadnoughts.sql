157d57ff1853efd66c986962b2d0d3b18320080e
Boris <59342149+CENTneRMOB@users.noreply.github.com>
2024-10-24 10:49:01 +0400

    109 populate the database with test data (#166)
    * #109 added storages migration
    
    * #109 added reagents migration

---
+++ packages/be/drizzle/0018_serious_dreadnoughts.sql
@@ -0,0 +1,32 @@
+-- Custom SQL migration file, put you code below! --
+INSERT INTO
+	storages (room, NAME, description)
+VALUES
+	('Room 1376', 'Top right shelf', ''),
+	('Room 1376', 'Bottom shelf right', ''),
+	('Room 1376', 'Bottom left shelf', ''),
+	('Room 1376', 'Cabinet 1, box 1', ''),
+	('Room 1376', 'Cabinet 1, box 2', ''),
+	('Room 1376', 'Freezer in refregirator', ''),
+	('Room 1376', 'Cabinet 2, shelf 1', ''),
+	('Room 1376', 'Cabinet 2, shelf 2', ''),
+	('Room 1376', 'Refregirator, shelf 1', ''),
+	('Room 1376', 'Refregirator, shelf 2', ''),
+	('Room 1376', 'Refregirator, door shelf', ''),
+	('Room 1376', 'Refregirator, shelf 3', ''),
+	('Room 1376', 'Refregirator, shelf 4', ''),
+	('Room 1376', 'Fumehood 1, left box', ''),
+	('Room 1374', 'Fumehood, right box', ''),
+	('Room 1374', 'Fumehood, left box', ''),
+	('Room 1374', 'Refrigerator 2, freezer', ''),
+	('Room 1370', 'Refrigirator, shelf 1, red box', ''),
+	(
+		'Room 1370',
+		'Refrigirator, shelf 1, green box',
+		''
+	),
+	('Room 1370', 'Refrigirator, left box', ''),
+	('Room 1370', 'Cabinet 1 shelf 1', ''),
+	('Room 1370', 'Solvents box', ''),
+	('Room 1374', 'Solvents box', ''),
+	('Room 1376', 'Solvents box', '');
