5bb4fc9d1dccd7f1e0eeebdedce2f608c806bd0f
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2024-10-29 18:10:23 +0400

    45 added filtration and pagination in mock

--- packages/fe/spec/mocks/substances.js
+++ packages/fe/spec/mocks/substances.js
@@ -5,6 +5,26 @@ import { reagents } from './reagents.js';
 
 const substances = [...reagents, ...samples];
 
+function filterSubstances(parsedOptions) {
+	const filteredSubstances = substances.filter(reagent => {
+		let matchesName = true;
+		let matchesQuantity = true;
+		let matchesLocation = true;
+
+		if (parsedOptions.name) {
+			matchesName = reagent.name.toLowerCase().includes(parsedOptions.name);
+		}
+		if (parsedOptions.quantity) {
+			matchesQuantity = parseInt(reagent.quantityLeft) === parsedOptions.quantity;
+		}
+		if (parsedOptions.location) {
+			matchesLocation = reagent.storageLocationId === parsedOptions.location;
+		}
+		return matchesName && matchesQuantity && matchesLocation;
+	});
+	return filteredSubstances;
+}
+
 export const substancesHandler = [
 	http.get(api('/substances'), req => {
 		const requestUrl = req.request.url;
@@ -12,19 +32,26 @@ export const substancesHandler = [
 		const productIds = url.searchParams.get('sort');
 		console.log(productIds);
 
+		const page = parseInt(url.searchParams.get('page')) || 1;
+		const limit = parseInt(url.searchParams.get('limit')) || 10;
+		const start = (page - 1) * limit;
+		const end = start + limit;
+
 		const options = url.searchParams.get('options');
 		const parsedOptions = JSON.parse(options);
-		const storageId = parsedOptions?.location;
-		if (storageId) {
-			const filteredSubstances = substances.filter(
-				substance => substance.storageLocationId === storageId
-			);
+
+		if (parsedOptions === null) {
+			const paginatedSubstances = substances.slice(start, end);
+			return HttpResponse.json({ paginatedSubstances, count: substances.length });
+		} else {
+			const filteredSubstances = filterSubstances(parsedOptions);
+			console.log(filteredSubstances);
+			const paginatedSubstances = filteredSubstances.slice(start, end);
 			return HttpResponse.json({
-				substances: filteredSubstances,
+				substances: paginatedSubstances,
 				count: filteredSubstances.length
 			});
 		}
-		return HttpResponse.json({ substances, count: substances.length });
 	}),
 
 	http.patch(api('/substances/:id'), async ({ request, params }) => {
