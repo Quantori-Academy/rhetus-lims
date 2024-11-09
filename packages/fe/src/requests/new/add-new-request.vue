d34b6cf711a81f059ebaf5567bca48d716d16f2e
Karina <umiralikarina@gmail.com>
2024-11-09 09:42:06 +0500

    232 added form for new request

--- packages/fe/src/requests/new/add-new-request.vue
+++ packages/fe/src/requests/new/add-new-request.vue
@@ -1,3 +1,56 @@
+<script setup>
+import { ref, useTemplateRef } from 'vue';
+import { ElForm, ElFormItem, ElInput, ElInputNumber, ElButton } from 'element-plus';
+import { requiredRule } from '../../reagents/details-reagent/constants';
+import { $router } from '../../lib/router/router';
+
+const formEl = useTemplateRef('form-el');
+const form = ref({
+	reagentName: '',
+	structure: '',
+	casNumber: '',
+	quantity: 1,
+	userComment: ''
+});
+const rules = ref({
+	reagentName: [requiredRule('Reagent Name')],
+	quantity: [requiredRule('Desired Quantity')]
+});
+
+const cancel = () => {
+	formEl.value.resetFields();
+	$router.push({ name: 'requests-list' });
+};
+</script>
+
 <template>
-	<h1>Create new request page</h1>
+	<div class="wrapper">
+		<el-form ref="form-el" :model="form" :rules="rules" label-width="auto" label-position="top">
+			<el-form-item label="Reagent Name" prop="reagentName">
+				<el-input v-model="form.reagentName" placeholder="Enter reagent name" />
+			</el-form-item>
+			<el-form-item label="Structure" prop="structure">
+				<el-input v-model="form.structure" placeholder="Enter structure" />
+			</el-form-item>
+			<el-form-item label="CAS number" prop="casNumber">
+				<el-input v-model="form.casNumber" placeholder="Indicate CAS number" />
+			</el-form-item>
+			<el-form-item label="Desired Quantity" prop="quantity">
+				<el-input-number v-model="form.quantity" :min="1" placeholder="Enter quantity" />
+			</el-form-item>
+			<el-form-item label="User Comment" prop="userComment">
+				<el-input v-model="userComment" type="textarea" placeholder="Enter comment" />
+			</el-form-item>
+			<div class="btn-container">
+				<el-button @click="cancel">Cancel</el-button>
+				<el-button type="primary">Create</el-button>
+			</div>
+		</el-form>
+	</div>
 </template>
+
+<style scoped>
+.el-input-number {
+	width: 100%;
+}
+</style>
