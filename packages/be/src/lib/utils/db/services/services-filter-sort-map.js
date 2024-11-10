987b4c2c198a231f139fe0e0cb2c0e09670430a4
Boris <6479500dev@gmail.com>
2024-11-11 03:31:45 +0400

    #193 added orders sorters and filters

--- packages/be/src/lib/utils/db/services/services-filter-sort-map.js
+++ packages/be/src/lib/utils/db/services/services-filter-sort-map.js
@@ -2,12 +2,14 @@ import { users } from './users.js';
 import { substances } from './substances.js';
 import { storages } from './storages.js';
 import { requests } from './requests.js';
+import { orders } from './orders.js';
 
 const servicesMapping = {
 	users,
 	substances,
 	storages,
-	requests
+	requests,
+	orders
 };
 
 export { servicesMapping };
