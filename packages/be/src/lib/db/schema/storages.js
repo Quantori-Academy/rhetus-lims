5bd15d0e157faf4730ef3304b6cc6bb346da5b69
Boris <6479500dev@gmail.com>
2024-11-14 15:09:58 +0400

    #207 updated db schema, added migration

--- packages/be/src/lib/db/schema/storages.js
+++ packages/be/src/lib/db/schema/storages.js
@@ -1,13 +1,19 @@
-import { pgTable, varchar, text, timestamp, boolean, uuid } from 'drizzle-orm/pg-core';
+import { pgTable, varchar, text, timestamp, boolean, uuid, unique } from 'drizzle-orm/pg-core';
 import { sql } from 'drizzle-orm';
 
-export const storages = pgTable('storages', {
-	id: uuid('id')
-		.primaryKey()
-		.default(sql`uuid_generate_v4()`),
-	room: varchar('room', { length: 300 }).notNull(),
-	name: varchar('name', { length: 300 }).notNull(),
-	description: text('description'),
-	createdAt: timestamp('created_at').notNull().defaultNow(),
-	deleted: boolean('deleted').default(false)
-});
+export const storages = pgTable(
+	'storages',
+	{
+		id: uuid('id')
+			.primaryKey()
+			.default(sql`uuid_generate_v4()`),
+		room: varchar('room', { length: 300 }).notNull(),
+		name: varchar('name', { length: 300 }).notNull(),
+		description: text('description'),
+		createdAt: timestamp('created_at').notNull().defaultNow(),
+		deleted: boolean('deleted').default(false)
+	},
+	table => ({
+		roomNameUniqueIndex: unique('storages_room_name_unique').on(table.room, table.name)
+	})
+);
