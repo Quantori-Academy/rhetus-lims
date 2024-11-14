5b31a6036ad0af7939c4bc7e0ce20eb8e44242d7
Boris <6479500dev@gmail.com>
2024-11-14 16:42:11 +0400

    #192 added cancelation route, validation schema

--- packages/be/src/routes/requests/requests-route.js
+++ packages/be/src/routes/requests/requests-route.js
@@ -2,6 +2,7 @@ import fp from 'fastify-plugin';
 
 import * as schema from './requests-schema.js';
 import requestsService from '../../services/requests/requests-service.js';
+import { RequestStatus } from '../../lib/db/schema/requests.js';
 
 async function requests(server, options) {
 	await server.register(requestsService);
@@ -162,6 +163,54 @@ async function requests(server, options) {
 			});
 
 			return reply.code(code).send({ status, message });
+		} catch (err) {
+			return reply.code(500).send(err);
+		}
+	}
+
+	server.route({
+		method: 'PUT',
+		path: options.prefix + 'requests/:id/cancel',
+		preValidation: [server.authenticate, server.officer],
+		schema: schema.cancelRequest,
+		handler: onCancelRequest
+	});
+
+	async function onCancelRequest(req, reply) {
+		try {
+			const authenticatedUserId = req.session.user.id;
+			const requestId = req.params.id;
+			const request = await server.requestsService.getRequestById(requestId);
+
+			if (!request) {
+				return reply.code(404).send({ status: 'error', message: `No such reagent request` });
+			}
+
+			if (request.status !== RequestStatus.PENDING) {
+				return reply.code(403).send({
+					status: 'error',
+					message: `Sorry. You cannot mark reagent request as canceled while it is in processing`
+				});
+			}
+
+			const isOfficer = await server.usersService.isOfficer(authenticatedUserId);
+
+			if (!isOfficer) {
+				return reply.code(403).send({
+					status: 'error',
+					message: `Sorry. You have no permissions to mark this reagent request as canceled`
+				});
+			}
+
+			const reagentName = await server.requestsService.cancelRequest(requestId, {
+				...req.body,
+				currentPoComment: request.poComment
+			});
+
+			return reply.code(200).send({
+				status: 'success',
+				message: `Request for reagent '${reagentName}' was marked as canceled`
+			});
 		} catch (err) {
 			return reply.code(500).send(err);
 		}
