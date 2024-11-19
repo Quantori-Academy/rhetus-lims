2197bab766fbf6857e49d086eadca9269fcc19f2
catherineisonline <ekaterine.jpg@gmail.com>
2024-11-19 13:42:19 +0530

    remove directly value mutations

--- packages/fe/src/orders/new/retrieve-data.js
+++ packages/fe/src/orders/new/retrieve-data.js
@@ -5,12 +5,13 @@ export const fetchSubstances = async (id, form, loading) => {
 	loading.value = true;
 	try {
 		const reagentData = await $api.reagents.fetchReagent(id);
-		form.value.reagents.push({
+		const newReagent = {
 			reagentName: reagentData.name,
 			quantityUnit: reagentData.quantityUnit,
 			quantity: reagentData.quantity,
 			amount: reagentData.amount || 1
-		});
+		};
+		form.value.reagents = [...form.value.reagents, newReagent];
 	} catch (error) {
 		$notifyUserAboutError(error.message || 'Error fetching reagent');
 	} finally {
@@ -24,7 +25,7 @@ export const fetchRequests = async (incomingRequests, loading) => {
 		const data = await $api.requests.fetchRequests();
 		incomingRequests.value = {
 			...data,
-			requests: data.requests.filter(request => request.status === 'pending')
+			requests: [...data.requests.filter(request => request.status === 'pending')] // Create a new array
 		};
 	} catch (error) {
 		$notifyUserAboutError(error.message || 'Error retrieving request');
