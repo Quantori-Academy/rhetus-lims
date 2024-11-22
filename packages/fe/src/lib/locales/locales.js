0dfb94f6510786321664f0ee9f93fd9dd64de07c
Karina <134139111+KaaRinnna@users.noreply.github.com>
2024-11-22 17:46:36 +0500

    299 Language section to profile settings (#311)
    * 299 added language section for profile
    
    * 299 moved locales to locales folder

---
+++ packages/fe/src/lib/locales/locales.js
@@ -0,0 +1,22 @@
+export const locales = [
+	{
+		value: 'en',
+		label: 'English'
+	},
+	{
+		value: 'ka',
+		label: 'Georgian'
+	},
+	{
+		value: 'hy',
+		label: 'Armenian'
+	},
+	{
+		value: 'kk',
+		label: 'Kazakh'
+	},
+	{
+		value: 'uk',
+		label: 'Ukrainian'
+	}
+];
