e8dd441b42f69443445293ce28b159dad9b7dfeb
Boris <6479500dev@gmail.com>
2024-11-23 12:03:20 +0400

    #294 updated substances route and validation schema

--- packages/be/src/routes/substances/substances-route.js
+++ packages/be/src/routes/substances/substances-route.js
@@ -1,10 +1,15 @@
 import fp from 'fastify-plugin';
 
 import * as schema from './substances-schema.js';
+import { helpers } from '../../lib/utils/common/helpers.js';
 import substancesService from '../../services/substances/substances-service.js';
+import reagentsService from '../../services/reagents/reagents-service.js';
+import samplesService from '../../services/samples/samples-service.js';
 
 async function substances(server, options) {
 	await server.register(substancesService);
+	await server.register(reagentsService);
+	await server.register(samplesService);
 
 	server.route({
 		method: 'GET',
@@ -25,60 +30,10 @@ async function substances(server, options) {
 	}
 
 	server.route({
-		method: 'PUT',
-		path: options.prefix + 'substances/quantity-change/:id',
-		preValidation: [server.authenticate],
-		schema: schema.changeQuantity,
-		handler: onQuantityChange
-	});
-
-	async function onQuantityChange(req, reply) {
-		try {
-			const { category, quantityUsed, reason } = req.body;
-
-			const substanceId = req.params.id;
-			const substance = await server.substancesService.getSubstanceById(substanceId, category);
-
-			if (!substance) {
-				return reply.code(404).send({ status: 'error', message: `No such ${category}` });
-			}
-
-			const canQuantityChange = await server.substancesService.canQuantityChange(
-				substanceId,
-				req.body
-			);
-
-			if (!canQuantityChange) {
-				return reply.code(409).send({
-					status: 'error',
-					message: `Quantity of ${category} '${substance.name}' cannot be changed. Check sending values`
-				});
-			}
-
-			const { code, status, message } = await server.substancesService.changeSubstanceQuantity(
-				substanceId,
-				{
-					category,
-					quantityUsed,
-					reason,
-					userId: req.session.user.id
-				}
-			);
-
-			return reply.code(code).send({
-				status,
-				message
-			});
-		} catch (err) {
-			return reply.code(500).send(err);
-		}
-	}
-
-	server.route({
 		method: 'PATCH',
 		path: options.prefix + 'substances/:id',
 		preValidation: [server.authenticate],
-		schema: schema.updateSubstanceSchema,
+		schema: schema.updateSubstance,
 		handler: onSubstanceUpdate
 	});
 
@@ -92,7 +47,9 @@ async function substances(server, options) {
 			const substanceId = req.params.id;
 			const substance = await server.substancesService.getSubstanceById(substanceId, category);
 			if (!substance) {
-				return reply.code(404).send({ status: 'error', message: `No such ${category}` });
+				return reply
+					.code(404)
+					.send({ status: 'error', message: `No such ${helpers.lowercase(category)}` });
 			}
 
 			const { isValid, codeStatus, errorMessage, updatedData } =
@@ -114,6 +71,110 @@ async function substances(server, options) {
 				status,
 				message
 			});
+		} catch (err) {
+			return reply.code(500).send(err);
+		}
+	}
+
+	server.route({
+		method: 'POST',
+		path: options.prefix + 'substances',
+		// preValidation: [server.authenticate],
+		schema: schema.createSubstance,
+		handler: onCreateSubstance
+	});
+
+	async function onCreateSubstance(req, reply) {
+		try {
+			const authenticatedUserId = req.session.user.id;
+			const { category, storageId, structure, components } = req.body;
+
+			const storage = await server.storagesService.getStorageById(storageId);
+
+			if (!storage) {
+				return reply.code(400).send({ status: 'error', message: `No such storage location` });
+			}
+
+			const isStructureValid = await server.substancesService.isStructureValid(structure || '');
+
+			if (!isStructureValid) {
+				return reply.code(400).send({ status: 'error', message: `Invalid structure` });
+			}
+
+			const areComponentsInsufficient = await server.substancesService.areComponentsInsufficient(
+				components,
+				category
+			);
+
+			if (areComponentsInsufficient) {
+				return reply.code(400).send({ status: 'error', message: `Not enough components left` });
+			}
+
+			const substanceName = await server.substancesService.createSubstance({
+				...req.body,
+				userId: authenticatedUserId
+			});
+
+			return reply.code(201).send({
+				status: 'success',
+				message: `${helpers.capitalize(category)} '${substanceName}' was created`
+			});
+		} catch (err) {
+			return reply.code(500).send(err);
+		}
+	}
+
+	server.route({
+		method: 'DELETE',
+		path: options.prefix + 'substances/:category/:id',
+		// preValidation: [server.authenticate],
+		schema: schema.deleteSubstance,
+		handler: onDeleteSubstance
+	});
+
+	async function onDeleteSubstance(req, reply) {
+		try {
+			const { category, id } = req.params;
+
+			const substance = await server.substancesService.getSubstanceById(id, category);
+
+			if (!substance) {
+				return reply
+					.code(404)
+					.send({ status: 'error', message: `No such ${helpers.lowercase(category)}` });
+			}
+
+			const substanceName = await server.substancesService.softDeleteSubstance(id, category);
+
+			return reply.code(200).send({
+				status: 'success',
+				message: `${helpers.capitalize(category)} '${substanceName}' was deleted`
+			});
+		} catch (err) {
+			return reply.code(500).send(err);
+		}
+	}
+
+	server.route({
+		method: 'GET',
+		path: options.prefix + 'substances/:category/:id',
+		// preValidation: [server.authenticate],
+		schema: schema.getSubstance,
+		handler: onGetSubstance
+	});
+
+	async function onGetSubstance(req, reply) {
+		try {
+			const { category, id } = req.params;
+			const substance = await server.substancesService.getSubstanceById(id, category);
+
+			if (!substance) {
+				return reply
+					.code(404)
+					.send({ status: 'error', message: `No such ${helpers.lowercase(category)}` });
+			}
+
+			return reply.code(200).send(substance);
 		} catch (err) {
 			return reply.code(500).send(err);
 		}
