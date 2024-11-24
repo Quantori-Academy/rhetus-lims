11d9a4056c7156f3359c78f4ddec1061ee754990
Boris <6479500dev@gmail.com>
2024-11-24 15:08:25 +0400

    #291 added default sort to params parse

--- packages/be/src/lib/utils/common/parse-params.js
+++ packages/be/src/lib/utils/common/parse-params.js
@@ -1,8 +1,10 @@
 function getClarifyParams(queryParams) {
+	const defaultSort = { default: 'desc' };
+
 	const limit = Number(queryParams.limit) || 10;
 	const page = Number(queryParams.page) || 1;
 	const options = queryParams.options ? JSON.parse(queryParams.options) : null;
-	const sort = queryParams.sort ? JSON.parse(queryParams.sort) : null;
+	const sort = queryParams.sort ? JSON.parse(queryParams.sort) : defaultSort;
 
 	const offset = page === 1 ? 0 : (page - 1) * limit;
 
