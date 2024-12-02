71b22b7438285be5724d4668dec2f8b468e862f1
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2024-12-03 00:39:10 +0400

    Updated time fnc

--- packages/fe/src/lib/utils/datetime/date-format.js
+++ packages/fe/src/lib/utils/datetime/date-format.js
@@ -2,6 +2,7 @@ import { format } from 'date-fns';
 
 // We show all dates in this format
 const DEFAULT_FORMAT = 'dd-MMM-yyyy';
+const TIMELINE_FORMAT = 'yyyy-MM-dd HH:mm';
 
 export const toDefaultFormat = date => format(date, DEFAULT_FORMAT);
 
@@ -12,14 +13,9 @@ export const formatDate = date => {
 };
 
 export const convertToCustomDate = originalTimestamp => {
+	if (!originalTimestamp) return '';
+
 	const date = new Date(originalTimestamp);
 
-	const year = date.getUTCFullYear();
-	const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
-	const day = date.getUTCDate().toString().padStart(2, '0');
-
-	const hours = date.getUTCHours().toString().padStart(2, '0');
-	const minutes = date.getUTCMinutes().toString().padStart(2, '0');
-
-	return `${year}-${month}-${day} ${hours}:${minutes}`;
+	return format(date, TIMELINE_FORMAT);
 };
